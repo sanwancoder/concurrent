026048775bc2de941c7821ac1ec18c09ee189f67
wuqing <wuqing@megvii.com>
2019-08-02 13:51:53 +0800

    a

--- src/yxxy/c_001/T.java
+++ src/yxxy/c_001/T.java
@@ -7,16 +7,27 @@
 package yxxy.c_001;
 
 public class T {
-	
+
 	private int count = 10;
 	private Object o = new Object();
-	
+
 	public void m() {
 		synchronized(o) { //任何线程要执行下面的代码，必须先拿到o的锁
 			count--;
 			System.out.println(Thread.currentThread().getName() + " count = " + count);
 		}
 	}
-	
+
+	public static void main(String[] args) {
+		T t = new T();
+
+		/*new Thread(()->t.m1(), "t1").start();
+		new Thread(()->t.m2(), "t2").start();*/
+
+		new Thread(t::m, "t1").start();
+		new Thread(t::m, "t2").start();
+	}
+
+
 }
 
--- src/yxxy/c_007/T.java
+++ src/yxxy/c_007/T.java
@@ -7,45 +7,44 @@ package yxxy.c_007;
 
 public class T {
 
-	public synchronized void m1() { 
+	public synchronized void m1() {
 		System.out.println(Thread.currentThread().getName() + " m1 start...");
 		try {
-			Thread.sleep(10000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		System.out.println(Thread.currentThread().getName() + " m1 end");
 	}
-	
+
+	/** Ã»ÓĞ synchronized */
 	public void m2() {
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		System.out.println(Thread.currentThread().getName() + " m2 ");
 	}
-	
+
+	public synchronized void m3() {
+		System.out.println(Thread.currentThread().getName() + " m3 start...");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + " m3 end");
+	}
+
 	public static void main(String[] args) {
 		T t = new T();
-		
-		/*new Thread(()->t.m1(), "t1").start();
-		new Thread(()->t.m2(), "t2").start();*/
-		
+
 		new Thread(t::m1, "t1").start();
 		new Thread(t::m2, "t2").start();
-		
-		/*
-		new Thread(new Runnable() {
+		new Thread(t::m3, "t3").start();
 
-			@Override
-			public void run() {
-				t.m1();
-			}
-			
-		});
-		*/
-		
+
 	}
-	
+
 }
